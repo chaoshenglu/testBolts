0fa3fc3d1f9fc7855b4190adf8f73ccb7b9e88cf
李响 <lixiangguge@gmail.com>
2020-04-24 20:25:45 +0800

    1

---
+++ testBolts/testBolts/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  Created by lixiang on 2020/4/24.
+//  Copyright © 2020 lixiang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame:UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController:ViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+
+
+}
+
---
+++ testBolts/testBolts/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Created by lixiang on 2020/4/24.
+//  Copyright © 2020 lixiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
